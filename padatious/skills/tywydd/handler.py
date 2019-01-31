8cd1079a1fe6fd798a149b527e6479337d3a25c0
Dewi Bryn Jones <d.b.jones@bangor.ac.uk>
2019-01-31 14:39:34 +0000

    dull creu ymateb yn tywydd. python string formatting syml

--- padatious/skills/tywydd/handler.py
+++ padatious/skills/tywydd/handler.py
@@ -8,11 +8,13 @@ class tywydd_handler:
 
     def handle(self, intent_parser_result):
         owm = pyowm.OWM('***REMOVED***')
+        context = intent_parser_result.matches
+
+        response = "Dyma'r tywydd gan OpenWeather ar gyfer {placename} "
+        result = response.format(**context)
 
         place = intent_parser_result.matches.get("placename")
         observation = owm.weather_at_place(place)
-        
-        result ="Dyma'r tywydd gan Open Weather ar gyfer %s \n" % place
         result = result + observation.get_weather().get_status()
 
         return result
