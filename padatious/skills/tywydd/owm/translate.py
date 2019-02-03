e85c5d7f9597a8bafaaba0a6ce43630770b2a203
Dewi Bryn Jones <d.b.jones@bangor.ac.uk>
2019-02-03 20:01:15 +0000

    cyfieithu canlyniadau o OWM

---
+++ padatious/skills/tywydd/owm/translate.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import os
+
+class Translator:
+
+    def __init__(self):
+       self._translations = dict()
+       self.initialize() 
+
+
+    def translate(self, own_field, text):
+        text = text.lower()
+        try:
+            return self._translations[own_field][text].strip()
+        except:
+            return text
+
+
+    def initialize(self):
+        trans_files_dir = os.path.dirname(os.path.realpath(__file__))
+        for trans_file in os.listdir(trans_files_dir):
+            if trans_file.endswith('.cy'):
+               field = trans_file.replace(".cy","")
+               self._translations[field] = self.read_file(os.path.join(trans_files_dir, trans_file))
+
+
+    def read_file(self, file_path):
+        translations = dict()
+        with open(file_path, 'r', encoding='utf-8') as trans_file:
+            for trans in trans_file:
+                en,cy = tuple(trans.split('|'))
+                en = en.lower()
+                cy = cy.lower()
+                translations[en]=cy
+        return translations 
+            
